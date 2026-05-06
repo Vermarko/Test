77de8e806d1f59381128d09df0ac105925b68432
Marco <130747172+Vermarko@users.noreply.github.com>
2026-05-06 11:17:12 +0200

    Update screenshot path in UnitTest1.cs

--- PlayWriteTests/UnitTest1.cs
+++ PlayWriteTests/UnitTest1.cs
@@ -28,7 +28,7 @@ namespace PlayWriteTests
             //
             await Page.ScreenshotAsync(new PageScreenshotOptions
              {
-                 Path = "screenshot.png",
+                 Path = ""./../../../Screenshot/screenshot.png",
                  FullPage = true
              });
             // locator cards
